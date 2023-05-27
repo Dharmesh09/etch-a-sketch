96eadf167072588fa0b4f9d13e442c806121a5ea
Dharmesh Mahajan <dharmeshmahajan2@gmail.com>
2023-05-27 19:33:23 +0530

    Make grids change color on hover

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.getElementById("container");
 
 const DEFAULT_SIZE = 16;
+let mouseDown = false;
 
 function createGrid(size) {
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -10,10 +11,20 @@ function createGrid(size) {
     for (let i = 0; i < total; i++) {
       const cell = document.createElement('div');
       cell.classList.add('cell');
+      cell.addEventListener('mouseover', changeColor);
+      cell.addEventListener('mousedown', changeColor);
       container.appendChild(cell);
     }
-  }
+}
 
+function changeColor(e) {
+    if (e.type === 'mouseover'){
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    }
+}
 // createGrid(DEFAULT_SIZE);
 
 window.onload = () => {
