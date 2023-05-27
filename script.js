4b7b40471d047e44e96b570ff879da074b4aab90
Dharmesh Mahajan <dharmeshmahajan2@gmail.com>
2023-05-27 19:17:39 +0530

    Create resizable grid

--- script.js
+++ script.js
@@ -1,35 +1,21 @@
 const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
 
-// console.log(container);
-// Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
+const DEFAULT_SIZE = 16;
+
+function createGrid(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let total = size*size;
+    for (let i = 0; i < total; i++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      container.appendChild(cell);
+    }
+  }
+
+// createGrid(DEFAULT_SIZE);
+
+window.onload = () => {
+    createGrid(DEFAULT_SIZE);
 }
-
-// Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
-
-    // Creates rows
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-        // console.log(r);
-    };
-};
-
-// Creates columns
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-            // console.log(j);
-        };
-
-    };
-};
-
-defaultGrid();
